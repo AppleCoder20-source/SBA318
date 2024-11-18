a2c1f2bae09b182fe9d28104e2c6b46e82d17def
Mashiur <56059603+AppleCoder20-source@users.noreply.github.com>
2024-11-18 14:51:04 -0500

    Added Starter Code

---
+++ data/quotes.js
@@ -0,0 +1,8 @@
+const quotes = [
+    {
+     id: 1,
+     quote: "I hated every minute of training, but I said, 'Don't quit. Suffer now and live the rest of your life as a champion.",
+     favorites: "yes",
+     author: "Muhammad Ali",
+    },
+]
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const quotes = require("./routes/quotes");
+
+// Use our Routes
+app.use("/author/quotes", quotes);
+app.use("/api/comments", comments);
+
+// Adding some HATEOAS links.
+app.get("/", (req, res) => {
+  res.json({
+    links: [
+      {
+        href: "/api",
+        rel: "api",
+        type: "GET",
+      },
+    ],
+  });
+});
+
+// Adding some HATEOAS links.
+app.get("/api", (req, res) => {
+  res.json({
+    links: [
+      {
+        href: "api/users",
+        rel: "users",
+        type: "GET",
+      },
+      {
+        href: "api/users",
+        rel: "users",
+        type: "POST",
+      },
+      {
+        href: "api/posts",
+        rel: "posts",
+        type: "GET",
+      },
+      {
+        href: "api/posts",
+        rel: "posts",
+        type: "POST",
+      },
+      {
+        href: "api/comments",
+        rel: "comments",
+        type: "GET",
+      },
+      {
+        href: "api/comments",
+        rel: "comments",
+        type: "POST",
+      },
+    ],
+  });
+});
+
+// 404 Middleware
+app.use((req, res, next) => {
+  next(error(404, "Resource Not Found"));
+});
+
+// Error-handling middleware.
+// Any call to next() that includes an
+// Error() will skip regular middleware and
+// only be processed by error-handling middleware.
+// This changes our error handling throughout the application,
+// but allows us to change the processing of ALL errors
+// at once in a single location, which is important for
+// scalability and maintainability.
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err.message });
+});
+
+app.listen(port, () => {
+  console.log(`Server listening on port: ${port}.`);
+});
+
+ 
